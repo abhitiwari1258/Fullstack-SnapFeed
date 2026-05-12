a68b8ee779c8e127f794aa27b9e3ee5e2ace90f3
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-05-12 22:54:07 +0530

    error fixed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
+// setServers() is part of the standard DNS module behavior
+// dns/promises is primarily for async DNS queries, not configuration in some Node versions
+
+const dns = require("node:dns/promises");
+dns.setServers(["8.8.8.8","1.1.1.1"])
 const express = require("express")
+const dotenv = require('dotenv');
+dotenv.config();
 const mongoose = require("mongoose")
 const cors = require("cors")
-const Router = require('./routes/postRoutes')
+// const Router = require('./routes/postRoutes')
 const postRoutes = require("./routes/postRoutes")
 
 const app = express()
@@ -11,7 +18,7 @@ const port = 5000
 
 app.use("/post",postRoutes)
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniInsta")
+mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log("mongoose connected"))
 .catch((err)=> console.log(err))
 
